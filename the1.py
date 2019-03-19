c79ae64366f00ea4c2183601b86d5088ab640593
free-bit <e2099430@ceng.metu.edu.tr>
2019-03-19 21:11:52 +0300

    Various changes

--- the1.py
+++ the1.py
@@ -6,9 +6,21 @@ import matplotlib.pyplot as plt          #Plots
 def normalize(data):
     return data/np.linalg.norm(data, ord=1)
 
-def grayscaleHistogram(data, bin_size=10, low=0, high=255):
-    freqs, bins=np.histogram(data, bins=bin_size, range=(low,high))
-    return (normalize(freqs), bins)
+def grayscaleHistogram(image2D, bin_size):
+    step=255/bin_size
+    bins=[step*i for i in range(bin_size+1)]
+    shape=image2D.shape
+    row_size=range(shape[0])
+    col_size=range(shape[1])
+    gray_hist=np.zeros(bin_size)
+    for i in row_size:
+        row=image2D[i]
+        for j in col_size:
+          index=int(row[j]/step)
+          if(index==bin_size):
+              index-=1
+          gray_hist[index]+=1
+    return (normalize(gray_hist), bins)
 
 def getCoordinates(bins):
     x=[]
@@ -17,7 +29,7 @@ def getCoordinates(bins):
     return x
     
 def plotHistogram(x_vals, weights, keys, **kwargs):
-    color="0504aa"
+    color="black"
     plot="bar"
     if "color" in kwargs:
         color=kwargs["color"] 
@@ -25,7 +37,7 @@ def plotHistogram(x_vals, weights, keys, **kwargs):
         plot=kwargs["plot"]
     if(plot is "bar"):
         plt.hist(x=x_vals, weights=weights, bins=keys,
-                     rwidth=0.75, alpha=0.7, color=color)
+                 rwidth=0.75, alpha=0.7, color=color)
     elif(plot is "line"):
         plt.plot(x_vals, weights, color)
     plt.grid(axis='y', alpha=0.75)
@@ -36,31 +48,34 @@ def plotHistogram(x_vals, weights, keys, **kwargs):
     plt.xticks(keys)
     #plt.show()
 
-def colorHistogram(r_vec, g_vec, b_vec, bin_size):
+def colorHistogram(r_vec2D, g_vec2D, b_vec2D, bin_size):
     step=255/bin_size
     color_hist=np.zeros((bin_size, bin_size, bin_size))
-    shape=r_vec.shape
+    shape=r_vec2D.shape
     row_size=range(shape[0])
     col_size=range(shape[1])
     for i in row_size:
+        r_row=r_vec2D[i]
+        g_row=g_vec2D[i]
+        b_row=b_vec2D[i]
         for j in col_size:
-          r=int(r_vec[i][j]/step)
+          r=int(r_row[j]/step)
           if(r==bin_size):
               r-=1
-          g=int(g_vec[i][j]/step)
+          g=int(g_row[j]/step)
           if(g==bin_size):
               g-=1
-          b=int(b_vec[i][j]/step)
+          b=int(b_row[j]/step)
           if(b==bin_size):
               b-=1
           color_hist[r,g,b]+=1
     return color_hist
 
-def partitionImage(image, level):
+def partitionImage(image2D, level):
     if(level==1):
-        return [image]
+        return [image2D]
     factor=2**(level-1)
-    sub_height, sub_width,_=image.shape
+    sub_height, sub_width,_=image2D.shape
     
     remainder_height=sub_height%factor
     sub_height=int(sub_height/factor)
@@ -76,7 +91,7 @@ def partitionImage(image, level):
         if(remainder_height>0):
             row_end+=1
             remainder_height-=1
-        rows=image[row_start:row_end]
+        rows=image2D[row_start:row_end]
         for j in iterations:                
             col_start=j*sub_width
             col_end=col_start+sub_width
@@ -87,7 +102,7 @@ def partitionImage(image, level):
             partitions.append(cols)  
     return partitions
 
-def applyGradient(image, **kwargs):
+def applyGradient(image2D, **kwargs):
     filters={"centered": {"horizontal": np.array([1,0,-1]), 
                           "vertical": np.array([1,0,-1]).reshape(3,1)},
              "backward": {"horizontal": np.array([0,1,-1]), 
@@ -106,7 +121,7 @@ def applyGradient(image, **kwargs):
     d_filter=filters[gradType][orient]   
     d_filter=np.broadcast_to(d_filter, (3, 3))
     #Apply convolution
-    return signal.convolve2d(image, d_filter, mode='same')
+    return signal.convolve2d(image2D, d_filter, mode='same')
 
 def findMagnitudesAndAngles(h_filtered, v_filtered):
     shape=h_filtered.shape
@@ -140,11 +155,11 @@ def gradientHistogram(magnitudes, angles, bin_size=9):
 
 def test1():#gray hist
     gray_face = misc.face(gray=True)
-    color_face = misc.face()
-    r=color_face[:,:,0].ravel()
-    g=color_face[:,:,1].ravel()
-    b=color_face[:,:,2].ravel()
-    freqs, bins=grayscaleHistogram(gray_face)
+    #color_face = misc.face()
+    #r=color_face[:,:,0].ravel()
+    #g=color_face[:,:,1].ravel()
+    #b=color_face[:,:,2].ravel()
+    freqs, bins=grayscaleHistogram(gray_face, 10)
     x_vals=getCoordinates(bins)
     plotHistogram(x_vals, freqs, bins)
     
@@ -153,9 +168,9 @@ def test2():#plot hist
     r=image[:,:,0]
     g=image[:,:,1]
     b=image[:,:,2]
-    freqs1, bins1=grayscaleHistogram(r.ravel(), 4)
-    freqs2, bins2=grayscaleHistogram(g.ravel(), 4)
-    freqs3, bins3=grayscaleHistogram(b.ravel(), 4)
+    freqs1, bins1=grayscaleHistogram(r, 4)
+    freqs2, bins2=grayscaleHistogram(g, 4)
+    freqs3, bins3=grayscaleHistogram(b, 4)
     x_vals1=getCoordinates(bins1)
     x_vals2=getCoordinates(bins2)
     x_vals3=getCoordinates(bins3)
@@ -190,7 +205,7 @@ def test5():#gradient
     plt.imshow(magnitudes)
         
 def main():
-    test3()
+    test5()
     
 if __name__ == "__main__":
     main()
